abdf5e3c5dbff3ad9b92fd6919f551d9fce91fde
DimitrisMel <dmelissourgos@yahoo.gr>
2022-07-31 21:26:02 -0400

    Add files via upload

---
+++ CMVariableCounter16.java
@@ -0,0 +1,240 @@
+package selfAdjustingCounterFast;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+import variableCounter.GeneralUtil;
+
+//import minusHLLestimator.GeneralUtil;
+
+
+/** A general framework for count min. The elementary data structures to be shared here can be counter, bitmap, FM sketch, HLL sketch. Specifically, we can
+ * use counter to estimate flow sizes, and use bitmap, FM sketch and HLL sketch to estimate flow cardinalities
+ * @author Jay, Youlin, 2018. 
+ */
+
+public class CMVariableCounter16 {
+	public static Random rand = new Random();
+	
+	public static int n = 0; 						// total number of packets
+	public static int flows = 0; 					// total number of flows
+	public static int avgAccess = 0; 				// average memory access for each packet
+	public static final int M = 1024 * 1024; 	// total memory space Mbits	
+	public static SAC64[][] C;
+	public static Set<Integer> sizeMeasurementConfig = new HashSet<>(Arrays.asList(0)); // -1-regular CM; 0-enhanced CM; 1-Bitmap; 2-FM sketch; 3-HLL sketch
+	public static Set<Integer> spreadMeasurementConfig = new HashSet<>(Arrays.asList()); // 1-Bitmap; 2-FM sketch; 3-HLL sketch
+	public static Set<Integer> expConfig = new HashSet<>(Arrays.asList()); //0-ECountMin dist exp
+	public static boolean isGetThroughput =false;
+	
+	/** parameters for count-min */
+	public static final int d =4; 			// the number of rows in Count Min
+	public static int w = 1;				// the number of columns in Count Min
+	public static int u = 1;				// the size of each elementary data structure in Count Min.
+	public static int[] S = new int[d];		// random seeds for Count Min
+	public static int m = 1;				// number of bit/register in each unit (used for bitmap, FM sketch and HLL sketch)
+
+	
+	/** parameters for counter */
+	public static int mValueCounter = 1;			// only one counter in the counter data structure
+	public static int counterSize = 36;				// size of each unit
+
+	/** parameters for bitmap */
+	public static final int bitArrayLength = 5000;
+	
+	/** parameters for FM sketch **/
+	public static int mValueFM = 128;
+	public static final int FMsketchSize = 32;
+	
+	/** parameters for HLL sketch **/
+	public static int mValueHLL = 128;
+	public static final int HLLSize = 5;
+
+	public static int times = 0;
+	
+	/** number of runs for throughput measurement */
+	public static int loops = 1;
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		/** measurement for flow sizes **/
+		if (isGetThroughput) {
+			return;
+		}
+		System.out.println("Start****************************");
+		/** measurement for flow sizes **/
+		
+		for (int i : sizeMeasurementConfig) {
+			times = 0;
+			
+				initCM(i);
+				//getThroughput();
+				encodeSize(GeneralUtil.dataStreamForFlowSize);
+				//long endTime = System.nanoTime();
+				//double duration = 1.0 * (endTime - startTime) / 1000000000;
+				//System.out.println("Average execution time: " + 1.0 * duration / loops + " seconds");
+				//System.out.println("Average Throughput: " + 1.0 * n / (duration / loops) + " packets/second" );
+	        	estimateSize(GeneralUtil.dataSummaryForFlowSize);
+	        	times++;
+			
+		}
+		
+		/** measurement for flow spreads **/
+		for (int i : spreadMeasurementConfig) {
+			initCM(i);
+		}
+		
+		/** experiment for specific requirement *
+		for (int i : expConfig) {
+			switch (i) {
+	        case 0:  initCM(0);
+					 encodeSize(GeneralUtil.dataStreamForFlowSize);
+					 randomEstimate(10000000);
+	                 break;
+	        default: break;
+			}
+		}*/
+		System.out.println("DONE!****************************");
+	}
+	
+	// Init the Count Min for different elementary data structures.
+	public static void initCM(int index) {
+		switch (index) {
+	        case 0: case -1: C = generateCounter();
+	                 break;
+	      
+	      
+	        default: break;
+		}
+		generateCMRamdonSeeds();
+		//System.out.println("\nCount Min-" + C[0][0].getDataStructureName() + " Initialized!");
+	}
+	
+	// Generate counter base Counter Min for flow size measurement.
+	public static SAC64[][] generateCounter() {
+		m = mValueCounter;
+		u = counterSize * mValueCounter;
+		w = (M / u) / d;
+		SAC64[][] B = new SAC64[d][w];
+		for (int i = 0; i < d; i++) {
+			for (int j = 0; j < w; j++) {
+				B[i][j] = new SAC64();
+			}
+		}
+		return B;
+	}
+	
+	// Generate bitmap base Counter Min for flow cardinality measurement.
+
+	
+	// Generate FM sketch base Counter Min for flow cardinality measurement.
+	
+	
+	// Generate random seeds for Counter Min.
+	public static void generateCMRamdonSeeds() {
+		HashSet<Integer> seeds = new HashSet<Integer>();
+		int num = d;
+		while (num > 0) {
+			int s = rand.nextInt();
+			if (!seeds.contains(s)) {
+				num--;
+				S[num] = s;
+				seeds.add(s);
+			}
+		}
+	}
+
+	/** Encode elements to the Count Min for flow size measurement. */
+	public static void encodeSize(String filePath) throws FileNotFoundException {
+		System.out.println("Encoding elements using " +  "s..." );
+		Scanner sc = new Scanner(new File(filePath));
+		n = 0;
+		
+		String[] flowida = new String[60000000];
+		while (sc.hasNextLine()) {
+			String entry = sc.nextLine();
+			String[] strs = entry.split("\\s+");
+			
+			String flowid = GeneralUtil.getSizeFlowID(strs, true);
+			
+			flowida[n]  = flowid;
+			n++;
+		}
+		int ii=0;
+		long startTime = System.nanoTime();	
+		while(ii<n) {
+			String flowid = flowida[ii];
+			ii++;
+			
+			
+			
+			
+			 for (int i = 0; i < d; i++) {
+                 int hashV = GeneralUtil.intHash(GeneralUtil.FNVHash1(flowid) ^ S[i]) ;
+                
+                
+                 int j = hashV >>>2;
+					j =(j % w + w) % w;
+					//j =0;
+				
+                 C[i][j].increaseValue(1, hashV & 3);
+                
+                 //if (n<100 && i==0 ) System.out.println(n +"\t"+C[i][j].getValue(0));
+                 
+                
+             }
+			 
+               
+			
+		}
+		long endTime = System.nanoTime();
+		double duration = 1.0 * (endTime - startTime) ;
+		System.out.println("processing time ns : "+duration/n);
+		System.out.println("Total number of encoded pakcets: " + n);
+		sc.close();
+	}
+
+	/** Estimate flow sizes. */
+	public static void estimateSize(String filePath) throws FileNotFoundException {
+		System.out.println("Estimating Flow SIZEs..." ); 
+		Scanner sc = new Scanner(new File(filePath));
+		String resultFilePath = GeneralUtil.path + "Results\\CM-SAC_" 
+				+ "_M_" +  M / 1024 / 1024 + "_d_" + d + "_u_" + u + "_m_" + m + "_w_" + w;
+		PrintWriter pw = new PrintWriter(new File(resultFilePath));
+		System.out.println("Result directory: " + resultFilePath); 
+		while (sc.hasNextLine()) {
+			String entry = sc.nextLine();
+			String[] strs = entry.split("\\s+");
+			String flowid = GeneralUtil.getSizeFlowID(strs, false);
+			//System.out.println("num is "+num);
+			//if (rand.nextDouble() <= GeneralUtil.getSizeSampleRate(num)) {
+			int[] value= new int[d];
+			if (true) {
+				int estimate = Integer.MAX_VALUE;
+				
+				for(int i = 0; i < d; i++) {
+					int hashV = GeneralUtil.intHash(GeneralUtil.FNVHash1(flowid) ^ S[i]);
+					int j = hashV>>>2;
+					j = (j % w + w) % w;
+					//j =0;
+					
+					
+					 value[i] =  C[i][j].getValue(hashV & 3);
+					
+					if (value[i] <estimate) estimate = value[i];
+				}
+				//Arrays.parallelSort(value);
+				//estimate = (d%2)==1?(value[(d-1)/2]):(value[d/2]+value[d/2-1])/2;
+				if (estimate<0) estimate =0;
+				pw.println(entry + "\t" + estimate);
+			}
+		}
+		sc.close();
+		pw.close();
+		// obtain estimation accuracy results
+		GeneralUtil.analyzeAccuracy(resultFilePath);
+	}
+}
---
+++ SAC64.java
@@ -0,0 +1,252 @@
+package selfAdjustingCounterFast;
+import java.util.BitSet;
+/** The implentation of conter */
+public class SAC64  {
+	/** parameters for counter */
+	public static String name = "VariableCounter";							// counter data structure name
+											// the number of counters in the counter array
+							
+	public BitSet I, C;
+	public SAC64() {		
+		I = new BitSet(4);
+		C = new BitSet(32);
+	}
+	public  int getBitSetValue(BitSet b) {
+		int res = 0;
+		for (int i = 0; i < b.length(); i++) {
+			if (b.get(i)) res += Math.pow(2, i);
+		}
+		return res;
+	}
+	public  int getBitSetValue(int start, int end) {
+		int res = 0;
+		for (int i = start; i <= end; i++) {
+			if (C.get(i)) res += Math.pow(2, i-start);
+		}
+		return res;
+	}
+	
+	public  void setByteValue(int value, int index) {
+		if (value<0) C.clear(index*8);
+		else C.set(index*8);
+		int i=1;
+		int tempV = Math.abs(value);
+		while  ( i<=7) {
+			if ((tempV & 1) !=0)
+				C.set(i+index*8);
+			else 
+				C.clear(i+index*8);
+			tempV = tempV >>> 1;
+			i++;
+		}
+	}
+	public void setShortValue(int value, int index) {
+		if (value<0) C.clear(0+index*16);
+		else C.set(0+index*16);
+		int i=1;
+		int tempV = Math.abs(value);
+		while  (i<=15) {
+			if ((tempV & 1) !=0)
+				C.set(i+index*16);
+			else 
+				C.clear(i+index*16);
+			tempV = tempV >>> 1;
+			i++;
+		}
+	}
+	public void setIntegerValue(int value) {
+		if (value<0) C.clear(0);
+		else C.set(06);
+		int i=1;
+		int tempV = Math.abs(value);
+		while  (i<=31) {
+			if ((tempV & 1) !=0)
+				C.set(i);
+			else 
+				C.clear(i);
+			tempV = tempV >>> 1;
+			i++;
+		}
+	}
+	
+	
+	public  int getValue(int index) {
+		if (I.get(1)) {
+			int value = getBitSetValue(1,31);
+			if (!C.get(0))
+				value = -value;
+			
+			return value;
+		}
+		else {
+			if (I.get((index/2)*2))
+			{
+				int value = getBitSetValue(1+(index/2)*16, 15+(index/2)*16);
+				if (!C.get((index/2)*16))
+					value = -value;
+				return value;
+			}
+			else {
+				int value = getBitSetValue(1+(index)*8, 7+(index)*8);
+				if (!C.get(index * 8))
+					value = -value;
+				return value;
+			}
+		}
+		
+	}
+	public void increaseByteByDelta(int delta, int index) {
+		if (delta==1 && C.get(index * 8)) {
+			int i = 1 + index * 8;
+			while (C.get(i) && i<=7 +index * 8) {
+				C.clear(i);
+				i++;
+			}
+			if (i <=7+index * 8) {
+				C.set(i);
+			}
+			else {
+				setShortValue(getValue(index/2*2+ 1-index%2)+128, index/2);
+				I.set((index/2)*2);
+			}
+		}
+		else {
+			if (delta==-1 && !C.get(index * 8)) {
+				int i = 1 + index * 8;
+				while (C.get(i) && i<=7 +index * 8) {
+					C.clear(i);
+					i++;
+				}
+				if (i <=7+index * 8) {
+					C.set(i);
+				}
+				else {
+					setShortValue(getValue(index/2*2+ 1-index%2)-128, index/2);
+					I.set((index/2)*2);
+				}
+			}
+			else {
+				if (delta==1 && !C.get(index * 8)) {
+					int i = 1 + index * 8;
+					while (!C.get(i) && i<=7 +index * 8) {
+						C.set(i);
+						i++;
+					}
+					if (i <=7+index * 8) {
+						C.clear(i);
+					}
+					else {
+						C.set(index * 8);
+						C.set(1+index * 8);
+						for (i=2+index*8; i<=7+index*8; i++)
+							C.clear(i);
+					}
+				}
+				else {
+					int i = 1 + index * 8;
+					while (!C.get(i) && i<=7 +index * 8) {
+						C.set(i);
+						i++;
+					}
+					if (i <=7+index * 8) {
+						C.clear(i);
+					}
+					else {
+						C.clear(index * 8);
+						C.set(1+index * 8);
+						for (i=2+index*8; i<=7+index*8; i++)
+							C.clear(i);
+					}
+				}
+			}
+		}
+	}
+	public void increaseShortByDelta(int delta, int index) {
+		if (delta==1 && C.get(0+index*16)) {
+			int i = 1+index*16 ;
+			while (C.get(i) && i<=15+index*16) {
+				C.clear(i);
+				i++;
+			}
+			if (i <=15+index*16) {
+				C.set(i);
+			}
+			else {
+				
+				I.set(1);
+				setIntegerValue(32768);
+			}
+		}
+		else {
+			if (delta==-1 && !C.get(0+index*16)) {
+				int i = 1+index*16;
+				while (C.get(i) && i<=15+index*16) {
+					C.clear(i);
+					i++;
+				}
+				if (i <=15+index*16) {
+					C.set(i);
+				}
+				else {
+					I.set(1);
+					setIntegerValue(-32768);
+				}
+			}
+			else {
+				if (delta==1 && !C.get(0+index*16)) {
+					int i = 1+index*16;
+					while (!C.get(i) && i<=15+index*16) {
+						C.set(i);
+						i++;
+					}
+					if (i <=15+index*16) {
+						C.clear(i);
+					}
+					else {
+						C.set(0);
+						C.set(1);
+						for (i=2+index*16; i<=15+index*16; i++)
+							C.clear(i);
+					}
+				}
+				else {
+					int i = 1+index*16;
+					while (!C.get(i) && i<=15+index*16) {
+						C.set(i);
+						i++;
+					}
+					if (i <=15+index*16) {
+						C.clear(i);
+					}
+					else {
+						C.clear(0);
+						C.set(1);
+						for (i=2+index*16; i<=15+index*16; i++)
+							C.clear(i);
+					}
+				}
+			}
+		}
+	}
+	public void increaseValue(int delta, int index) {
+		if (I.get(1)) {
+			int value = getBitSetValue(1,31);
+			if (!C.get(0))
+				value = -value;
+			value += delta;
+			setIntegerValue(value);
+		}
+		else {
+			if (I.get((index/2)*2))
+			{
+				increaseShortByDelta(delta, index/2);
+			}
+			else {
+				increaseByteByDelta(delta, index);
+
+			}
+		}
+		
+	}
+	
+}
