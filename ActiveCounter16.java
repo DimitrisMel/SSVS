5aa16783ea3b7be4503deb1f3cd28b4b0205caf5
DimitrisMel <dmelissourgos@yahoo.gr>
2022-08-08 15:56:21 -0400

    Update and rename VariableCounter16.java to ActiveCounter16.java

--- ActiveCounter16.java
+++ ActiveCounter16.java
@@ -2,12 +2,11 @@ package virtualActiveCounter;
 import java.util.BitSet;
 import java.util.HashSet;
 import java.util.Random;
-/** The implentation of conter */
-public class VariableCounter16  {
+/** The implentation of Active counter */
+public class ActiveCounter16  {
 	/** parameters for counter */
 	public static String name = "VariableCounter";							// counter data structure name
-											// the number of counters in the counter array
-								
+											// the number of counters in the counter array					
 	public BitSet  C;
 	public VariableCounter16() {
 	
@@ -22,15 +21,11 @@ public class VariableCounter16  {
 		return res;
 	}
 
-	
 	public  int getValue(int index) {
 		
 			int exp = getBitSetValue(4,7);
 			int absV = getBitSetValue(0,3);
 			 return (int)(absV * Math.pow(2, exp)+Math.pow(2, 4+exp)-16);
-			
-		
-		
 	}
 	
 	public void increaseExp(int delta) {
@@ -71,7 +66,5 @@ public class VariableCounter16  {
 		if (rand.nextInt(expexp)==0)
 					increaseValue(delta);
 			
-		
 	}
-	
 }
