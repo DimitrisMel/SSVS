ae40e58c401c0b20b2a5938062d1c4a81708d982
DimitrisMel <dmelissourgos@yahoo.gr>
2022-08-08 15:54:58 -0400

    Update and rename virtualActiveCounter.java to RCS_AC.java

--- RCS_AC.java
+++ RCS_AC.java
@@ -1,4 +1,3 @@
-package virtualActiveCounter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
@@ -9,22 +8,14 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
-//import minusHLLestimator.GeneralUtil;
-
-
-/** A general framework for count min. The elementary data structures to be shared here can be counter, bitmap, FM sketch, HLL sketch. Specifically, we can
- * use counter to estimate flow sizes, and use bitmap, FM sketch and HLL sketch to estimate flow cardinalities
- * @author Jay, Youlin, 2018. 
- */
-
-public class virtualActiveCounter {
+public class RCS_AC {
 	public static Random rand = new Random();
 	
 	public static int n = 0; 						// total number of packets
 	public static int flows = 0; 					// total number of flows
 	public static int avgAccess = 0; 				// average memory access for each packet
 	public static final int M = 1024 * 1024 * 1; 	// total memory space Mbits	
-	public static VariableCounter16[][] C;
+	public static ActiveCounter16[][] C;
 	public static Set<Integer> sizeMeasurementConfig = new HashSet<>(Arrays.asList(0)); // -1-regular CM; 0-enhanced CM; 1-Bitmap; 2-FM sketch; 3-HLL sketch
 	public static Set<Integer> spreadMeasurementConfig = new HashSet<>(Arrays.asList()); // 1-Bitmap; 2-FM sketch; 3-HLL sketch
 	public static Set<Integer> expConfig = new HashSet<>(Arrays.asList()); //0-ECountMin dist exp
@@ -71,30 +62,11 @@ public class virtualActiveCounter {
 			times = 0;
 			
 				initCM(i);
-				//getThroughput();
 				encodeSize(GeneralUtil.dataStreamForFlowSize);
-				//long endTime = System.nanoTime();
-				//double duration = 1.0 * (endTime - startTime) / 1000000000;
-				//System.out.println("Average execution time: " + 1.0 * duration / loops + " seconds");
-				//System.out.println("Average Throughput: " + 1.0 * n / (duration / loops) + " packets/second" );
 	        	estimateSize(GeneralUtil.dataSummaryForFlowSize);
 	        	times++;
 			
 		}
-		
-		/** measurement for flow spreads **/
-		
-		
-		/** experiment for specific requirement *
-		for (int i : expConfig) {
-			switch (i) {
-	        case 0:  initCM(0);
-					 encodeSize(GeneralUtil.dataStreamForFlowSize);
-					 randomEstimate(10000000);
-	                 break;
-	        default: break;
-			}
-		}*/
 		System.out.println("DONE!****************************");
 	}
 	
@@ -108,7 +80,6 @@ public class virtualActiveCounter {
 	        default: break;
 		}
 		generateCMRamdonSeeds();
-		//System.out.println("\nCount Min-" + C[0][0].getDataStructureName() + " Initialized!");
 	}
 	
 	// Generate counter base Counter Min for flow size measurement.
@@ -124,12 +95,6 @@ public class virtualActiveCounter {
 		}
 		return B;
 	}
-	
-	// Generate bitmap base Counter Min for flow cardinality measurement.
-
-	
-	// Generate FM sketch base Counter Min for flow cardinality measurement.
-	
 	
 	// Generate random seeds for Counter Min.
 	public static void generateCMRamdonSeeds() {
@@ -218,8 +183,6 @@ public class virtualActiveCounter {
 					estimate += value[i];
 					
 				}
-				//Arrays.parallelSort(value);
-				//estimate = (d%2)==1?(value[(d-1)/2]):(value[d/2]+value[d/2-1])/2;
 				estimate =(int)( (w*d)*1.0/(w-d)*(  estimate/d-n/w)   ) ;
 				if (estimate<0) estimate =0;
 				pw.println(entry + "\t" + estimate);
