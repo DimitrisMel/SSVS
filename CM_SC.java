19b4048d9fe3e6e284e56ec55caef7feb4843c14
DimitrisMel <dmelissourgos@yahoo.gr>
2022-08-08 15:30:32 -0400

    Update and rename CM-SC.java to CM_SC.java

--- CM_SC.java
+++ CM_SC.java
@@ -9,7 +9,7 @@ import java.util.Set;
 
 import variableCounter.GeneralUtil;
 
-public class CM-SC {
+public class CM_SC {
 	public static Random rand = new Random();
 	
 	public static int n = 0; 						// total number of packets
